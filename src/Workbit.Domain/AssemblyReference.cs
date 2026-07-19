46568ebdb1c6a442b9975ebd235f5fea99407a3b
Boris Anastasov <boris_ywu@abv.bg>
2026-07-19 10:22:57 +0300

    Add static assembly references

---
+++ src/Workbit.Domain/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Workbit.Domain
+{
+    public static class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    }
+}
+
